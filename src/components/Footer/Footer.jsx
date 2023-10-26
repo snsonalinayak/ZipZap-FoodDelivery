178db0484d5fd8618b991ebc9e9425c35bcfbe5f
snsonalinayak@gmail.com <89394473+snsonalinayak@users.noreply.github.com>
2023-10-26 13:16:53 +0530

    Initial Commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+
+import "../../styles/footer.css";
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className=" footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>ZipZap</h5>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt pariatur accusamus
+              </p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delivery Time</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Sunday - Thursday</span>
+                <p>10:00am - 11:00pm</p>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Friday - Saturday</span>
+                <p>Off day</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <p>Location: Jaipur,Rajasthan</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Phone: 0989825367</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Email: example@gmail.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+        </Row>
+
+        
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
